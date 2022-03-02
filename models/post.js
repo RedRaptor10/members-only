95a71e886c727e4b3b61029bd1bccfe497ab1bb4
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-02 15:04:56 -0600

    Created models

---
+++ models/post.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+    title: { type: String, minLength: 1, maxLength: 100, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    date: { type: Date },
+    text: { type: String, minLength: 1, required: true }
+});
+
+PostSchema
+.virtual('url')
+.get(function() {
+    return '/post/' + this._id;
+});
+
+module.exports = mongoose.model('Post', PostSchema);
---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username: { type: String, minLength: 1, maxLength: 20, required: true },
+    password: { type: String, minLength: 5, required: true },
+    firstName: { type: String, maxLength: 20 },
+    lastName: { type: String, maxLength: 20 },
+    status: { type: String, required: true }
+});
+
+UserSchema
+.virtual('url')
+.get(function() {
+    return '/user/' + this._id;
+});
+
+module.exports = mongoose.model('User', UserSchema);

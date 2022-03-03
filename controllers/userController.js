e76111daad58c6a74c00b1ee708f4b587c79aa96
RedRaptor10 <weiminh_lam@yahoo.com>
2022-03-03 13:08:08 -0600

    Added Join page

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const nconf = require("nconf");
 
 // Sign Up
 exports.signUp = [
@@ -24,12 +25,12 @@ exports.signUp = [
             password: req.body.password,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            status: 'member'
+            status: 'Member'
         });
 
         if (!errors.isEmpty()) {
             // Error, render form again with sanitized values/error messages
-            res.render('signUp', { title: 'Sign Up', formData: user, errors: errors.array() });
+            res.render('signUp', { title: 'Sign Up', user: req.user, formData: user, errors: errors.array() });
         } else {
             // Check if username already exists
             User.findOne({'username': user.username})
@@ -37,7 +38,7 @@ exports.signUp = [
                 if (err) { return next(err); }
                 else if (results) {
                     // Username exists, render form again
-                    res.render('signUp', { title: 'Sign Up', formData: user,
+                    res.render('signUp', { title: 'Sign Up', user: req.user, formData: user,
                         errors: [{ msg: 'Username already exists.'}] });
                 } else {
                     // Form data is valid. Hash password and save user info to database
@@ -49,7 +50,7 @@ exports.signUp = [
                         user.save(function(err) {
                             if (err) { return next(err); }
                             // Success. Render sign up page with account created
-                            res.render('signUp', { title: 'Sign Up', accountCreated: true });
+                            res.render('signUp', { title: 'Sign Up', user: req.user, accountCreated: true });
                         });
                     });
                 }
@@ -58,6 +59,7 @@ exports.signUp = [
     }
 ];
 
+// Log In
 exports.logIn = [
     // Validate and sanitize fields
     body('username', 'Username required.').trim().isLength({ min: 1 }).escape(),
@@ -70,14 +72,14 @@ exports.logIn = [
 
         if (!errors.isEmpty()) {
             // Error, render form again with sanitized values/error messages
-            res.render('logIn', { title: 'Log In', formData: { username: req.body.username }, errors: errors.array() });
+            res.render('logIn', { title: 'Log In', user: req.user, formData: { username: req.body.username }, errors: errors.array() });
         } else {
             passport.authenticate('local', function(err, user, info) {
                 //successRedirect: '/',
                 //failureRedirect: '/log-in'
                 if (err) { return next(err); }
                 // Incorrect username and/or password. Render form again
-                if (!user) { return res.render('logIn', { title: 'Log In', formData: { username: req.body.username },
+                if (!user) { return res.render('logIn', { title: 'Log In', user: req.user, formData: { username: req.body.username },
                     errors: [{ msg: info.message }] }) }
 
                 // Success. Log in & redirect to homepage
@@ -90,8 +92,34 @@ exports.logIn = [
     }
 ];
 
+// Log Out
 exports.logOut = (req, res) => {
     const delay = 5;
     req.logout();
     res.render('logOut', { title: 'Log Out', delay: delay, message: 'You have been successfully logged out. You will be redirected in ' + delay + ' seconds.' });
 }
+
+// Join
+exports.join = [
+    // Validate and sanitize fields
+    body('password', 'Password required.').trim().isLength({ min: 1 }).escape(),
+
+    // Process Join
+    (req, res, next) => {
+        // Extract the validation errors from request
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Error, render form again with sanitized values/error messages
+            res.render('join', { title: 'Join', user: req.user, errors: errors.array() });
+        } else if (req.body.password != nconf.get('MEMBERS_PASSWORD')) {
+            res.render('join', { title: 'Join', user: req.user, errors: [{ msg: 'Incorrect password.' }] });
+        } else {
+            User.updateOne({"username": req.user.username}, {"$set": {"status": "Avenger"}})
+            .exec(function(err) {
+                if (err) { return next(err); }
+                res.render('join', { title: 'Join', user: req.user, joined: true });
+            });
+        }
+    }
+];
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,31 @@ var router = express.Router();
 // Controller Modules
 var userController = require('../controllers/userController');
 
-/* GET home page. */
+/* Home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Avengers', user: req.user });
+  res.render('index', { title: 'The Avengers', user: req.user });
 });
 
-/* Sign Up Form GET */
+/* Sign Up Form */
 router.get('/sign-up', function(req, res) {
-  res.render('signUp', { title: 'Sign Up' });
+  res.render('signUp', { title: 'Sign Up', user: req.user });
 });
-
-/* Sign Up Form POST */
 router.post('/sign-up', userController.signUp);
 
-/* Log In Form GET */
+/* Log In Form */
 router.get('/log-in', function(req, res) {
-  res.render('logIn', { title: 'Log in' });
+  res.render('logIn', { title: 'Log in', user: req.user });
 });
-
-/* Log In Form POST */
 router.post('/log-in', userController.logIn);
 
-/* Log Out GET */
+/* Log Out */
 router.get('/log-out', userController.logOut);
+
+/* Join */
+router.get('/join', function(req, res) {
+  res.render('join', { title: 'Join', user: req.user });
+});
+router.post('/join', userController.join);
 
 /* Post */
 //router.get('/create-post', function(req, res) {
